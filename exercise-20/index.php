78c9462ca25c5873c7ba75617331556854093e9d
Boyan Mihailov <mihailovy@gmail.com>
2025-01-27 17:25:22 +0200

    Add exercise 20

---
+++ exercise-20/index.php
@@ -0,0 +1,92 @@
+<?php
+
+function cacheWrite($url, $data)
+{
+    $filename = rawurlencode($url).".json"; 
+    file_put_contents($filename, $data); 
+}
+
+function cacheRead($url, $modify = "")
+{
+    $filename = rawurlencode($url).".json"; 
+    $data = file_get_contents($filename);
+}
+
+function xml_encode($data)
+{
+	$xml  = '<?xml version="1.0" encoding="UTF-8"?>';
+	$xml .= "<users>";
+	foreach($data as $i => $iv) {
+		$xml = $xml . "<user>";	
+		foreach($iv  as $j => $jv) {
+			$xml .= "<" .$j.">";
+			$xml .= $jv;
+			$xml .= "</".$j.">";
+		}
+		$xml = $xml."</user>";
+	}
+	$xml .= "</users>";
+	return $xml;
+}
+
+
+function xml_decode($data)
+{
+}
+
+// Свързване към база данни   
+$db = new mysqli("localhost", "root", "");
+
+// Избор на конкретна база данни
+mysqli_select_db($db, "test");
+
+if (isset($_GET['out']) and  $_GET['out'] == "xml") {
+	$mode = "xml";
+} else {
+	$mode = "json";
+}
+
+if (isset($_GET['c'])) {
+    if ( $_GET['c'] == "read"){
+		// Does cache exist?
+		if (file_exists(rawurlencode("c=read"))) {
+			$json = cacheRead("c=read");
+		} else {
+			$query = mysqli_query($db, "SELECT * FROM `user`");
+	        $user = [];
+	        while($res = mysqli_fetch_assoc($query)) {
+	            $user[] = $res;
+	        };
+	        if ($mode == "xml"){
+				header("Content-type: text/xml");
+				$json = xml_encode($user);
+			} else {
+				header("Content-type: text/json");
+				$json = json_encode($user);
+			}
+	        cacheWrite("c=read", $json);
+		}
+        echo($json);
+        
+    }
+    if ( $_GET['c'] == "create"){
+        $name   = $_GET['name']; 
+        $family = $_GET['family'];
+        $email  = $_GET['email'];
+        $query = mysqli_query($db, "
+          INSERT INTO `user`
+                (`id`,`name`, `family`,`email`)
+            VALUES
+                (null, '$name', '$family', '$email') ");
+        echo( json_encode("success"));
+
+    }
+    if ( $_GET['c'] == "delete"){
+        $id   = $_GET['id']; 
+        $query = mysqli_query($db, "
+          DELETE FROM `user`
+            WHERE `id` = $id");
+        echo( json_encode("success"));
+    }
+}
+
